9bcaf486243c8c366f2d6d78c27b71be4791c97f
RyutaGoto <b1016230@fun.ac.jp>
2022-02-16 23:56:43 +0900

    replaced get controller

--- app/app.js
+++ app/app.js
@@ -10,11 +10,13 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000; //port番号を指定
 
 // GET http://localhost:3000/api/v1/
-app.get('/api/v1/', function(req, res){
-    res.json({
-        message: 'Hello World'
-    });
-});
+// app.get('/api/v1/', function(req, res){
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+const router = require('./routes/v1/');
+app.use('/api/v1/', router);
 
 // サーバ起動
 app.listen(port);
